5fb97f5a78d46dbde2a31423e030af05d85242c1
Jens1234567890 <43136836+Jens1234567890@users.noreply.github.com>
2024-08-29 21:03:28 +0200

    Update Program.cs

--- ConsoleApp9GitV2/Program.cs
+++ ConsoleApp9GitV2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! GitV2");
+            Console.WriteLine("Hello, World! GitV2Hub");
         }
     }
 }
